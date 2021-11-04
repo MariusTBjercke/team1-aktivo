c1d9811b8191a4e0a7c68c49c668d112015119a0
MariusTBjercke <marius@bjerckemedia.no>
2021-11-04 08:54:14 +0100

    Initial commit

---
+++ js/model.js
@@ -0,0 +1,21 @@
+const aktivo = {
+
+    // App
+    app: {
+        currentPage: '',
+        user: '',
+    }
+
+    // Input
+    inputs: {
+
+    }
+
+    // Data
+    data: {
+        options: {
+            theme: 0 // 0 for light, 1 for dark?
+        }
+    }
+
+}
